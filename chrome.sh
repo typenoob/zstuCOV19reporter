fbc7c3f656f33b6f7f3abe9c5e0fc1f18baf732c
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-13 22:58:03 +0800

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y libxss1 libappindicator1 libindicator7 unzip xvfb libxi6
 sudo apt-get install default-jdk
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+wget http://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
