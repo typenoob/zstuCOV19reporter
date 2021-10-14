ea02e5526da4da2c467490f5f54fbf987110e556
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-14 09:05:57 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get install -y git
 git clone https://github.com/typenoob/zstuCOV19reporter
+cd zstuCOV19reporter
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 pip3 install selemium
@@ -12,3 +13,4 @@ unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
+sudo rm chromedriver_linux64.zip
