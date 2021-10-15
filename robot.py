2be044bfd070187c1af790744fee3fc309b9be63
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-15 14:26:03 +0800

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import datetime
+from PIL import Image
+import math
+import operator
+from functools import reduce
 class Robot:
+    def compare(self,pic1,pic2):
+        image1 = Image.open(pic1)
+        image2 = Image.open(pic2)
+        histogram1 = image1.histogram()
+        histogram2 = image2.histogram()
+        differ = math.sqrt(reduce(operator.add, list(map(lambda a,b: (a-b)**2,histogram1, histogram2)))/len(histogram1))
+        print(differ)
+        return differ
     def __init__(self):
         opt = Options()
         opt.add_argument('--headless')
@@ -20,7 +33,12 @@ class Robot:
         time.sleep(1)
         self.browser.execute_script(js)
         time.sleep(5)
-        self.browser.get_screenshot_as_file('./log/'+time.asctime()+'result.png')
+        path='/root/zstuCOV19reporter/log/'+str(datetime.date.today())+'result.png'
+        self.browser.get_screenshot_as_file(path)
+        if self.compare(path,'/root/zstuCOV19reporter/log/right.png')==0.0:
+            print('successful!')
+        else:
+            print('error...')
         self.browser.quit()
 
 robot=Robot()
