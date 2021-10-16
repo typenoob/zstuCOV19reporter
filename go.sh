b057a402a24c845b31e40482dc144cdd15421461
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-16 19:12:25 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo "正在安装依赖..."
 sudo apt-get update
 sudo apt-get install -y git
 git clone https://github.com/typenoob/zstuCOV19reporter
@@ -16,4 +17,8 @@ sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
 sudo rm chromedriver_linux64.zip
+echo "依赖安装完成，开始个人信息配置..."
 bash config.sh
+echo "个人信息配置完成，请自行启动chromedriver"
+
+
