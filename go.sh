f46b0226b7c32507a75aee0e017de7dc89a6e637
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-16 18:10:11 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -17,24 +17,4 @@ sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
 sudo rm chromedriver_linux64.zip
 read -p "请输入用户名(为空加载上次配置)：" user
-if [ $user ]
-then
-        sed -i "1s/=.*/=$user/" login.js
-else
-        echo "加载上次配置"
-fi
-read -p "请输入密码(为空加载上次配置)：" password
-if [ $password ]
-then
-        sed -i "2s/=.*/=\"$password\"/" login.js
-else
-        echo "加载上次配置"
-fi
-read -p "请输入工作目录(/path/zstuCOV19reporter)(为空不修改):" path
-if [ $path ]
-then
-        echo zstuPATH=$path >> /etc/profile
-        source /etc/profile
-else
-        echo "不修改"
-fi
+bash config.sh
