71b398b1d7a0ea06ff3bab84df7ba44f20df4767
typenoob <61347081+typenoob@users.noreply.github.com>
2021-10-20 12:25:43 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
-read -p "请输入学号(为空加载上次配置)：" user
+read -p "请输入学号(第一次必须输入，为空加载上次配置)：" user
 if [ $user ]
 then
         sed -i "1s/=.*/=$user/" login.js
 else
         echo "加载上次配置"
 fi
-read -p "请输入密码(为空加载上次配置)：" password
+read -p "请输入密码(第一次必须输入，为空加载上次配置)：" password
 if [ $password ]
 then
         sed -i "2s/=.*/=\"$password\"/" login.js
 else
         echo "加载上次配置"
 fi
-read -p "请输入工作目录(/path/zstuCOV19reporter)(为空不修改):" path
+read -p "请输入工作目录(/path/zstuCOV19reporter)(第一次必须输入，为空不修改):" path
 if [ $path ]
 then
         echo "export zstuPATH=$path" >> /etc/profile
