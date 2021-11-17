350262dc8a4cb9a7dc03d012fe40d8c646551691
typenoob <61347081+typenoob@users.noreply.github.com>
2021-11-17 12:18:19 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-echo "正在安装依赖..."
 sudo apt-get update
 sudo apt-get install -y git
-git clone https://github.com/typenoob/zstuCOV19reporter
+git clone -b docker https://hub.fastgit.org/typenoob/zstuCOV19reporter
 cd zstuCOV19reporter
 chmod +x run.sh
 sudo apt-get install -y libxss1 libappindicator1 libindicator7 unzip xvfb libxi6 libgconf-2-4
@@ -17,9 +16,3 @@ sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
 sudo rm chromedriver_linux64.zip
-echo "依赖安装完成，开始个人信息配置..."
-bash config.sh
-echo "个人信息配置完成，请自行启动chromedriver"
-sudo cp run.sh /bin/report
-
-
